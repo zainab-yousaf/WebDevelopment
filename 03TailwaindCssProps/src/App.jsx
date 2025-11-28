f0810f38b55eed406cc549579140f1334138e3c5
zainabyousaf_ <91596027+zainab-yousaf@users.noreply.github.com>
2025-11-29 04:24:28 +0500

    tailwaind Css abd props

--- 03TailwaindCssProps/src/App.jsx
+++ 03TailwaindCssProps/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-
+import Card from "./Components/Card";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Tailwain Css</h1>
+      <h1 className="bg-green-700 text-black p-4 rounded-3xl mb-7">
+        Tailwain Test
+      </h1>
+      <Card userName="Zainab" btnClick="Read more" />
+      <br />
+      <br />
+      <Card userName="CodeView" btnClick="Visit more" />
     </>
   );
 }
